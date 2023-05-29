e5abe2480c41ae191f19f6ac667a2225308e9e50
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-29 16:01:07 +0530

    change header in serverless

--- enviornment/api/serverless.js
+++ enviornment/api/serverless.js
@@ -1,5 +1,7 @@
 const stringify = require("json-stringify-safe");
 
 export default function handler(request, response) {
+  const newHeaders = new Headers(request.headers);
+  console.log(newHeaders);
   return response.json(stringify(request));
 }
