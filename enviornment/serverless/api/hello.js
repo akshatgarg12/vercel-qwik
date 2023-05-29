353a56c4cdecbaa24efba180b91533e248ea5d33
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-29 15:03:36 +0530

    added safe json serialiser

--- enviornment/serverless/api/hello.js
+++ enviornment/serverless/api/hello.js
@@ -1,3 +1,5 @@
+const stringify = require("json-stringify-safe");
+
 export default function handler(request, response) {
-  return response.json(request);
+  return response.json(stringify(request));
 }
