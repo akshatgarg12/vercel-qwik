9bd311a9bb0c5fb44407d9069ad8ec9ff72fcc76
akshatgarg12 <akshatarungarg78@gmail.com>
2023-05-29 16:04:41 +0530

    debug serverlesS

--- enviornment/api/serverless.js
+++ enviornment/api/serverless.js
@@ -3,5 +3,9 @@ const stringify = require("json-stringify-safe");
 export default function handler(request, response) {
   const newHeaders = new Headers(request.headers);
   console.log(newHeaders);
-  return response.json(stringify(request));
+  const edgeRequestObject = {
+    ...request,
+    headers: newHeaders,
+  };
+  return response.json(JSON.parse(stringify(edgeRequestObject)));
 }
